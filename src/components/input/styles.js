b570c5876fb417a12003bea1e0081da96e731c70
Bruna <brunamata@usp.br>
2022-12-04 21:44:09 -0300

    Tela de login

---
+++ src/components/input/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    widgh: 100%;
+    max-width: 275px;
+    height: 30px;
+    border-bottom: 1px solid #383450
+
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+
+
+`
+
+export const IconContainer = styled.div`
+    margin-right: 10px;
+`
+
+export const InputText = styled.input`
+    background-color: transparent;
+    color: #ffffff;
+    border: 0;
+    width: 100%; /* pro input pegar 100% da largura */
+    height: 30px;
+`
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,12 +1,42 @@
+
 import {Link} from "react-router-dom"
+import { Button } from "../../components/button";
+import { Header } from "../../components/header";
+import { Input } from "../../components/input";
+
+import{Container, TextContent, Title, Column, 
+CriarText, EsqueciText, Row, SubTitleLogin, TitleLogin, Wrapper} from './styles';
+
 
 const Login = () => {
     return (<>
-        <h1>Login</h1>
-        <Link to="/">Voltar para home</Link>
+        <Header />
+        <Container>
+            <Column>
+                <Title>
+                    A plataforma para você aprender com experts, dominar as principais tecnologias
+                    e entrar mais rápido nas empresas mais desejadas.
+                </Title>
+            </Column>
+            <Column>
+                <Wrapper>
+                    <TitleLogin>Faça seu cadastro</TitleLogin>
+                    <SubTitleLogin>Faça seu login e make the change._</SubTitleLogin>
+                    <form>
+                        <Input placeholder="E-mail"/>
+                        <Input placeholder="Senha" type="password"/>
+                        <Button title="Entrar" variant="secondary" />
+                    </form>
+                    <Row>
+                        <EsqueciText>Esqueci minha senha</EsqueciText>
+                        <CriarText>Criar Conta</CriarText>
+                    </Row>
+                </Wrapper>
+            </Column>
+        </Container>
     </>
 
-     )
+     ) 
 } 
- 
+
 export {Login};
